96c9698f97d40b75c0172385b5268ae8431823e2
AndriiPopov <andriy.popov.vl@gmail.com>
2019-02-01 18:23:31 +0100

    Add support to db

--- client/build/precache-manifest.c161b918b34da5168835a6256037b796.js
+++ client/build/precache-manifest.c161b918b34da5168835a6256037b796.js
@@ -16,8 +16,8 @@ self.__precacheManifest = [
     "url": "/static/media/logo.a2b37daf.svg"
   },
   {
-    "revision": "b0c11c46be0b81ea4283",
-    "url": "/static/js/2.b0c11c46.chunk.js"
+    "revision": "3b00c9d33ffa8848a90c",
+    "url": "/static/js/2.3b00c9d3.chunk.js"
   },
   {
     "revision": "d22c45a7c9039f10affa",
@@ -36,8 +36,8 @@ self.__precacheManifest = [
     "url": "/static/js/main.d2c435a0.chunk.js"
   },
   {
-    "revision": "3db82a202e4a33ca5fb9",
-    "url": "/static/js/runtime~main.3db82a20.js"
+    "revision": "63123dff4deba7067061",
+    "url": "/static/js/runtime~main.63123dff.js"
   },
   {
     "revision": "98c6b9d25b216a4532da190ba832c7df",
@@ -48,19 +48,19 @@ self.__precacheManifest = [
     "url": "/static/media/visibility_off_hover.17320d74.svg"
   },
   {
-    "revision": "0cd95a97be67f5145af93d607471ae1d",
-    "url": "/static/media/visibility_on.0cd95a97.svg"
-  },
-  {
     "revision": "f8690bd917468699b72e4d849da2e048",
     "url": "/static/media/visibility_on_hover.f8690bd9.svg"
   },
   {
-    "revision": "b0c11c46be0b81ea4283",
+    "revision": "0cd95a97be67f5145af93d607471ae1d",
+    "url": "/static/media/visibility_on.0cd95a97.svg"
+  },
+  {
+    "revision": "3b00c9d33ffa8848a90c",
     "url": "/static/css/2.120b364b.chunk.css"
   },
   {
-    "revision": "33f0483b19fd518cf05051484188bb92",
+    "revision": "13d2dfa11b7ea576be69f4fa6dc975d6",
     "url": "/index.html"
   }
 ];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b10c2afb0792eacd00939155e218b295.js"
+  "/precache-manifest.c161b918b34da5168835a6256037b796.js"
 );
 
 workbox.clientsClaim();
