cc4578fa5fd48c804c1de59965ffe1b91788596c
AndriiPopov <andriy.popov.vl@gmail.com>
2019-12-03 20:33:21 +0100

    New

---
+++ client/build_editor/precache-manifest.5b0f0e1bcac653fa178139a275375aac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "89ac158fc69410f71a10",
+    "url": "/static/js/runtime~main.71106518.js"
+  },
+  {
+    "revision": "5a4080516f176fa89324",
+    "url": "/static/js/main.cf91e910.chunk.js"
+  },
+  {
+    "revision": "64786d979267bbdfc818",
+    "url": "/static/js/4.5af1f322.chunk.js"
+  },
+  {
+    "revision": "0af9986cca5ef03716f8",
+    "url": "/static/js/3.f37aa00e.chunk.js"
+  },
+  {
+    "revision": "91957015869051856ecc",
+    "url": "/static/js/2.0f7d99ba.chunk.js"
+  },
+  {
+    "revision": "5a4080516f176fa89324",
+    "url": "/static/css/main.d98ca625.chunk.css"
+  },
+  {
+    "revision": "64786d979267bbdfc818",
+    "url": "/static/css/4.5793161a.chunk.css"
+  },
+  {
+    "revision": "0af9986cca5ef03716f8",
+    "url": "/static/css/3.477b4c86.chunk.css"
+  },
+  {
+    "revision": "91957015869051856ecc",
+    "url": "/static/css/2.be18cd06.chunk.css"
+  },
+  {
+    "revision": "ee2b2faadc5464a03829abe628ab5d81",
+    "url": "/index.html"
+  }
+];
--- client/build_editor/service-worker.js
+++ client/build_editor/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef451942ef86b07aaba1ef0593a519a6.js"
+  "/precache-manifest.5b0f0e1bcac653fa178139a275375aac.js"
 );
 
 workbox.clientsClaim();
