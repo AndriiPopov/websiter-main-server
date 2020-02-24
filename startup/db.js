32fb0dec1a4ba2c31c93efea7cf954c502da381e
AndriiPopov <andriy.popov.vl@gmail.com>
2020-02-24 17:52:54 +0100

    dfsdfds

--- startup/db.js
+++ startup/db.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 
 module.exports = function() {
     try {
-        // const db = process.env.websiter_db
-        const db =
-            'mongodb://mainServer:20websiter20@ds321819-a0.mlab.com:21819,ds321819-a1.mlab.com:21819/websiter?replicaSet=rs-ds321819'
+        const db = process.env.websiter_db
+        // const db =
+        //     'mongodb://mainServer:20websiter20@ds321819-a0.mlab.com:21819,ds321819-a1.mlab.com:21819/websiter?replicaSet=rs-ds321819'
         return mongoose.connect(db).then(() => {
             // User.update({}, { currentAction: 0 }, { multi: true }, function(
             //     err,
