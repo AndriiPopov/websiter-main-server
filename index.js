1b0c26d0839b0fee7e1c9b115b068b4493f7d5d3
AndriiPopov <andriy.popov.vl@gmail.com>
2020-02-24 14:44:29 +0100

    ddsf

--- index.js
+++ index.js
@@ -13,42 +13,42 @@ const rateLimiterMiddleware = require('./middleware/rateLimiter')
 
 const app = express()
 // app.use(rateLimiterMiddleware)
-app.use(sslRedirect())
+// app.use(sslRedirect())
 
-const apiApp = express()
-apiApp.use(passport.initialize())
+// const apiApp = express()
+// apiApp.use(passport.initialize())
 
-const liveApp = express()
+// const liveApp = express()
 
-const myApp = express()
-myApp.use(passport.initialize())
+// const myApp = express()
+// myApp.use(passport.initialize())
 
-aws.config.region = 'us-east-2'
+// aws.config.region = 'us-east-2'
 
-logging()
-routes(app, myApp, liveApp, apiApp)
-db()
-validation()
+// logging()
+// routes(app, myApp, liveApp, apiApp)
+// db()
+// validation()
 
-prod(app)
-prod(liveApp)
-prod(apiApp)
-prod(myApp)
+// prod(app)
+// prod(liveApp)
+// prod(apiApp)
+// prod(myApp)
 
-app.engine('html', require('ejs').renderFile)
-liveApp.engine('html', require('ejs').renderFile)
-apiApp.engine('html', require('ejs').renderFile)
-myApp.engine('html', require('ejs').renderFile)
+// app.engine('html', require('ejs').renderFile)
+// liveApp.engine('html', require('ejs').renderFile)
+// apiApp.engine('html', require('ejs').renderFile)
+// myApp.engine('html', require('ejs').renderFile)
 
-app.use(express.static('./public'))
-liveApp.use(express.static('./public'))
-apiApp.use(express.static('./public'))
-myApp.use(express.static('./public'))
-//app.use(express.static(__dirname + '/public'))
+// app.use(express.static('./public'))
+// liveApp.use(express.static('./public'))
+// apiApp.use(express.static('./public'))
+// myApp.use(express.static('./public'))
+// //app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT
 const server = app.listen(port, () =>
     winston.info(`Listening on port ${port}...`)
 )
-connectSocket(server)
-module.exports = server
+// connectSocket(server)
+// module.exports = server
