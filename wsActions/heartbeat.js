0f3f52f79b6279835a0b96e245fe5234727224c1
AndriiPopov <andriy.popov.vl@gmail.com>
2020-02-25 10:50:52 +0100

    kjlkj

--- wsActions/heartbeat.js
+++ wsActions/heartbeat.js
@@ -16,52 +16,54 @@ module.exports.heartbeat = async (ws, data) => {
                         .required()
                         .validate(item)
                     if (!error) {
+                        let needToRequest
                         if (data.versions[item] && ws.resources[item]) {
-                            if (
-                                data.versions[item].__v !== ws.resources[item]
-                            ) {
-                                requestResource(
-                                    {
-                                        type: data.versions[item].type,
-                                        id: item,
-                                    },
-                                    ws
-                                )
-                                // const resource =
-                                //     data.versions[item].type === 'user'
-                                //         ? await User.findById(item)
-                                //         : data.versions[item].type === 'website'
-                                //         ? await Website.findById(item)
-                                //         : await Resource.findById(item)
-                                // if (resource) {
-                                //     ws.send(
-                                //         JSON.stringify({
-                                //             messageCode:
-                                //                 'hardupdate' +
-                                //                 data.versions[item].type,
-                                //             type: data.versions[item].type,
-                                //             resourceId: item,
-                                //             resource:
-                                //                 data.versions[item].type ===
-                                //                 'resource'
-                                //                     ? {
-                                //                           draft: !resource.draft
-                                //                               .structure
-                                //                               ? resource.published
-                                //                               : resource.draft,
-                                //                           present: {},
-                                //                           future: [],
-                                //                           past: [],
-                                //                           published: !resource.draft
-                                //                               .structure,
-                                //                           __v: resource.__v,
-                                //                           _id: resource._id,
-                                //                       }
-                                //                     : resource,
-                                //         })
-                                //     )
-                                // }
-                            }
+                            if (data.versions[item].__v !== ws.resources[item])
+                                needToRequest = true
+                        } else if (data.versions[item] && !ws.resources[item])
+                            needToRequest = true
+                        if (needToRequest) {
+                            requestResource(
+                                {
+                                    type: data.versions[item].type,
+                                    id: item,
+                                },
+                                ws
+                            )
+                            // const resource =
+                            //     data.versions[item].type === 'user'
+                            //         ? await User.findById(item)
+                            //         : data.versions[item].type === 'website'
+                            //         ? await Website.findById(item)
+                            //         : await Resource.findById(item)
+                            // if (resource) {
+                            //     ws.send(
+                            //         JSON.stringify({
+                            //             messageCode:
+                            //                 'hardupdate' +
+                            //                 data.versions[item].type,
+                            //             type: data.versions[item].type,
+                            //             resourceId: item,
+                            //             resource:
+                            //                 data.versions[item].type ===
+                            //                 'resource'
+                            //                     ? {
+                            //                           draft: !resource.draft
+                            //                               .structure
+                            //                               ? resource.published
+                            //                               : resource.draft,
+                            //                           present: {},
+                            //                           future: [],
+                            //                           past: [],
+                            //                           published: !resource.draft
+                            //                               .structure,
+                            //                           __v: resource.__v,
+                            //                           _id: resource._id,
+                            //                       }
+                            //                     : resource,
+                            //         })
+                            //     )
+                            // }
                         }
                     }
                 }
