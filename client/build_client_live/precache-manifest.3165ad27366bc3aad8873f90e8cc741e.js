99739e589ff2a595f5e06f6776fc2468fc6a2fed
AndriiPopov <andriy.popov.vl@gmail.com>
2020-03-07 06:50:56 +0100

    asdsa

---
+++ client/build_client_live/precache-manifest.3165ad27366bc3aad8873f90e8cc741e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e39d9d90570e619c027593749b7715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf7e5dea0158623de300",
+    "url": "/static/js/2.95b5bea1.chunk.js"
+  },
+  {
+    "revision": "0d3d9d93e45b40a36834",
+    "url": "/static/js/main.fb4a8045.chunk.js"
+  },
+  {
+    "revision": "416981186027e5ab00eb",
+    "url": "/static/js/runtime-main.195c9f28.js"
+  }
+]);
--- client/build_client_live/service-worker.js
+++ client/build_client_live/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.275da223d691a0b8ac7b5fea1af59e80.js"
+  "/precache-manifest.3165ad27366bc3aad8873f90e8cc741e.js"
 );
 
 self.addEventListener('message', (event) => {
