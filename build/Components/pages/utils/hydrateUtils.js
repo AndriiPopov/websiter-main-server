ab2d5ffaaabc6f35f306ae39043fda597988cb13
AndriiPopov <andriy.popov.vl@gmail.com>
2020-05-18 14:03:59 +0200

    dfgdf

--- build/Components/pages/utils/hydrateUtils.js
+++ build/Components/pages/utils/hydrateUtils.js
@@ -11,50 +11,45 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
-const migrateInnerChildren = (tags, props) => {// if (props.inEntry && document && document.body) {
-  //     for (let tag of tags) {
-  //         const tagPath = tag + props.elementsPath
-  //         const oldDiv = document.querySelector(
-  //             'div[websiterforprocessing="' + tagPath + '"]'
-  //         )
-  //         if (!oldDiv) return
-  //         let newDiv = document.querySelector(
-  //             'div[websiterforprocessing="' + tagPath + '_Temp"]'
-  //         )
-  //         if (!newDiv) {
-  //             newDiv = document.createElement('div')
-  //             newDiv.setAttribute('websiterforprocessing', tagPath + '_Temp')
-  //             newDiv.setAttribute(
-  //                 'style',
-  //                 'position:absolute;left:-10000px;top:-10000px;width:0px;height:0px;display:none;'
-  //             )
-  //             document.body.appendChild(newDiv)
-  //         }
-  //         while (oldDiv.childNodes.length > 0) {
-  //             newDiv.appendChild(oldDiv.childNodes[0])
-  //         }
-  //     }
-  // }
+const migrateInnerChildren = (tags, props) => {
+  if (props.inEntry && document && document.body) {
+    for (let tag of tags) {
+      const tagPath = tag + props.elementsPath;
+      const oldDiv = document.querySelector('div[websiterforprocessing="' + tagPath + '"]');
+      if (!oldDiv) return;
+      let newDiv = document.querySelector('div[websiterforprocessing="' + tagPath + '_Temp"]');
+
+      if (!newDiv) {
+        newDiv = document.createElement('div');
+        newDiv.setAttribute('websiterforprocessing', tagPath + '_Temp');
+        newDiv.setAttribute('style', 'position:absolute;left:-10000px;top:-10000px;width:0px;height:0px;display:none;');
+        document.body.appendChild(newDiv);
+      }
+
+      while (oldDiv.childNodes.length > 0) {
+        newDiv.appendChild(oldDiv.childNodes[0]);
+      }
+    }
+  }
 };
 
 exports.migrateInnerChildren = migrateInnerChildren;
 
-const returnInnerElements = (tags, props) => {// if (props.inEntry && document && document.body) {
-  //     for (let tag of tags) {
-  //         const tagPath = tag + props.elementsPath
-  //         const newDiv = document.querySelector(
-  //             'div[websiterforprocessing="' + tagPath + '"]'
-  //         )
-  //         const oldDiv = document.querySelector(
-  //             'div[websiterforprocessing="' + tagPath + '_Temp"]'
-  //         )
-  //         if (!oldDiv || !newDiv) return
-  //         while (oldDiv.childNodes.length > 0) {
-  //             newDiv.appendChild(oldDiv.childNodes[0])
-  //         }
-  //         oldDiv.parentElement.removeChild(oldDiv)
-  //     }
-  // }
+const returnInnerElements = (tags, props) => {
+  if (props.inEntry && document && document.body) {
+    for (let tag of tags) {
+      const tagPath = tag + props.elementsPath;
+      const newDiv = document.querySelector('div[websiterforprocessing="' + tagPath + '"]');
+      const oldDiv = document.querySelector('div[websiterforprocessing="' + tagPath + '_Temp"]');
+      if (!oldDiv || !newDiv) return;
+
+      while (oldDiv.childNodes.length > 0) {
+        newDiv.appendChild(oldDiv.childNodes[0]);
+      }
+
+      oldDiv.parentElement.removeChild(oldDiv);
+    }
+  }
 };
 
 exports.returnInnerElements = returnInnerElements;
